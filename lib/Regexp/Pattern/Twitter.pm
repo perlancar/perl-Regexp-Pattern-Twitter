4c80ab7fe29bdbb8055ccd184898e8423a17da88
perlancar (on netbook-zenbook-ux305) <perlancar@gmail.com>
2018-09-10 19:31:16 +0700

    Add examples

--- lib/Regexp/Pattern/Twitter.pm
+++ lib/Regexp/Pattern/Twitter.pm
@@ -4,7 +4,14 @@ package Regexp::Pattern::Twitter;
 # VERSION
 
 our %RE = (
-    username => { pat => qr/[0-9A-Za-z_]{1,15}/ },
+    username => {
+        pat => qr/[0-9A-Za-z_]{1,15}/,
+        examples => [
+            {str=>'perlancar', matches=>1},
+            {str=>'a234567890123456', gen_args=>{-anchor=>1}, matches=>0, summary=>'Too long'},
+            {str=>'perl ancar', gen_args=>{-anchor=>1}, matches=>0, summary=>'Contains whitespace'},
+        ],
+    },
 );
 
 1;
